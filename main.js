731fa9fd65ddce1258b3e40f7508e0378f657bfe
Jonabrakib <hrakib435@gmail.com>
2020-06-17 11:48:16 +0600

    tab layout done

---
+++ main.js
@@ -0,0 +1,16 @@
+var tabButtons=document.querySelectorAll(".hero .tab-btn button");
+var tabPanels=document.querySelectorAll(".hero  .tab");
+
+function showPanel(panelIndex,colorCode) {
+    tabButtons.forEach(function(node){
+        node.style.backgroundColor="";
+        node.style.color="";
+    });
+    tabButtons[panelIndex].style.backgroundColor=colorCode;
+    tabButtons[panelIndex].style.color="white";
+    tabPanels.forEach(function(node){
+        node.style.display="none";
+    });
+    tabPanels[panelIndex].style.display="block";
+}
+showPanel(0,'#f44336');
